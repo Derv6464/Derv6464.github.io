caf810af084fcdaa4a98403698c9130a05624c22
Derv6464 <dervlag6464@gmail.com>
2025-10-07 18:13:39 +0100

    spelling oops

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@
       <h1>Hey, I'm Dervla</h1>
       <p>I'm currently:</p>
       <ul>
-        <li>Studing <u>Immserive Software Engineering</u> @ UL</li>
+        <li>Studing <u>Immersive Software Engineering</u> @ UL</li>
         <li>Working part time @ Analog Devices</li>
         <li>Co-Head of ULAS HiPR, Univeristy of Limerick's High Power Rocketry team </li>
       </ul>
